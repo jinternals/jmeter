f6d7dac3388ecebbab8f7046fa6296553c1c4d0c
mradulpandey <mradul@metadesignsolutions.in>
2016-08-11 17:54:21 +0530

    Changes

--- src/test/jmeter/script/jmeter_dashboard_runner.sh
+++ src/test/jmeter/script/jmeter_dashboard_runner.sh
@@ -1,10 +1,18 @@
 #!/usr/bin/env bash
 
 JMETER_HOME="$1"
-
 RESULT_FILE="$2"
 EXPORT_DASHBOARD_HOME="$3"
 
+
+echo Environment Variables :
+echo -------------------------------------------------------------------------------------------------------
+
+echo JMETER_HOME : $JMETER_HOME
+echo RESULT_FILE : $RESULT_FILE
+echo EXPORT_DASHBOARD_HOME : $EXPORT_DASHBOARD_HOME
+
+echo -------------------------------------------------------------------------------------------------------
 
 rm -r $EXPORT_DASHBOARD_HOME
 mkdir -p $EXPORT_DASHBOARD_HOME
